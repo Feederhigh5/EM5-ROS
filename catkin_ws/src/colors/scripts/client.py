b26f185444d4aff1a541e2617e2751e39af4f3c9
Feederhigh5 <super-ruben@web.de>
2022-10-25 10:54:42 +0200

    service and client working

--- catkin_ws/src/colors/scripts/client.py
+++ catkin_ws/src/colors/scripts/client.py
@@ -2,18 +2,25 @@
 import sys
 import rospy
 
-import colors.srv import *
+from colors.srv import *
 
-def get_position(color):
-    rospy.wait_for_service('get_position')
-    try:
-        get_position = rospy.ServiceProxy('get_position', GetPosition)
-        response = get_position(color)
-        return response
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+class Client:
+
+    def __init__(self, color):
+        self.color = color
+
+    @staticmethod
+    def get_position(color):
+        rospy.wait_for_service('get_position')
+        try:
+            get_position = rospy.ServiceProxy('get_position', GetPosition)
+            response = get_position(color)
+            return response
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
 
 
 if __name__ == "__main__":
-    print("Requesting %s"%color)
-    print("%s in Position %i"(color, get_position(color)))
+    client=Client("RED")
+    print("Requesting %s"%client.color)
+    print("%s in Position %s"%(client.color, client.get_position(client.color)))
--- catkin_ws/src/colors/scripts/service.py
+++ catkin_ws/src/colors/scripts/service.py
@@ -1,21 +1,27 @@
 
 from colors.srv import GetPosition,GetPositionResponse
 import rospy
+import random
 
 class Service:
 
     def __init__(self):
-        service = rospy.Service('get_position', GetPosition, return_position)
-        self.color = ["GREEN", "RED", "BLUE"]
+        service = rospy.Service('get_position', GetPosition, self.return_position)
+        self.colors = ["GREEN", "RED", "BLUE"]
         self.randomize_array()
+        rospy.loginfo("array %s"%self.colors)
 
-    def return_position(req):
-        return self.color.indexOf(req.color);
+    def return_position(self, req):
+        rospy.loginfo('Returning position of %s in %s'%(req.color, self.colors))
+        pos = self.colors.index(req.color)+1;
+        return GetPositionResponse(pos)
 
     def randomize_array(self):
-        while not rospy.is_shutdown():
-            random.shuffle(self.color)
+        # while not rospy.is_shutdown():
+        random.shuffle(self.colors)
 
 
 if __name__ == '__main__':
     rospy.init_node('position_service')
+    Service()
+    rospy.spin()
